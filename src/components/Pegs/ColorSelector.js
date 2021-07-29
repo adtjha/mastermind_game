4b7f309148b713210a67e71c3a0e1f09de8c198e
adtjha <adtjha@hotmail.com>
2021-07-29 18:32:08 +0530

    fix build issues

--- src/components/Pegs/ColorSelector.js
+++ src/components/Pegs/ColorSelector.js
@@ -5,19 +5,19 @@ import { getSelectedColor } from "../Store/selectors";
 export const ColorSelector = (props) => {
   const dispatch = useDispatch();
   const selected = useSelector(getSelectedColor);
-  let style =
-    "w-8 h-8 rounded-full p-4 my-4 mx-auto cursor-pointer " +
+  let classTags =
+    "w-8 h-8 rounded-full p-4 my-4 mx-auto cursor-pointer" +
     " bg-" +
     props.color +
     "-400";
 
   if (selected === props.color) {
-    style += " border-2 border-black";
+    classTags += " border-2 border-black";
   }
 
   return (
     <div
-      className={style}
+      className={classTags}
       onClick={(e) => dispatch(update_selected(props.color))}
     ></div>
   );
